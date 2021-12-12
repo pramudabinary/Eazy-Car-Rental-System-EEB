b8d08d5153691ea6a1e0dd85990b6d3e9910139d
Pramuda Liyanage <pramudatharika@gmail.com>
2021-12-13 00:08:25 +0530

    apis add

---
+++ src/main/java/lk/ijse/spring/SpringBootAppInitializer.java
@@ -0,0 +1,8 @@
+package lk.ijse.spring;
+
+/**
+ * @author Pramuda Liyanage <pramudatharika@gmail.com>
+ * @since 12/12/21
+ **/
+public class SpringBootAppInitializer {
+}
---
+++ src/main/java/lk/ijse/spring/dto/CustomerDTO.java
@@ -0,0 +1,8 @@
+package lk.ijse.spring.dto;
+
+/**
+ * @author Pramuda Liyanage <pramudatharika@gmail.com>
+ * @since 12/12/21
+ **/
+public class CustomerDTO {
+}
